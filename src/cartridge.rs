2c202e4c234f8e14aaf1083b120e1a2a757653bc
GLodi <lodi.giulio@gmail.com>
2020-09-29 16:06:36 +0200

    cartridge is read from commandline argument

---
+++ src/cartridge.rs
@@ -0,0 +1,24 @@
+#[derive(Clone, Debug)]
+pub struct Cartridge {
+    pub rom: Vec<u8>,
+}
+
+impl Cartridge {
+    pub fn new(filename: &str) -> Cartridge {
+        match std::fs::read(filename) {
+            Ok(bytes) => Cartridge { rom: bytes },
+            Err(e) => {
+                if e.kind() == std::io::ErrorKind::NotFound {
+                    eprintln!(
+                        "ERROR OPENING {}: are you sure the filename is correct?",
+                        filename
+                    );
+                }
+                if e.kind() == std::io::ErrorKind::PermissionDenied {
+                    eprintln!("please run again with appropriate permissions.");
+                }
+                panic!("{}", e);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use std::env;
 use std::time::Duration;
 
+mod cartridge;
 mod cpu;
 mod display;
 mod input;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("trying to open {:?}", &args[1]);
+
     let mut k = input::Keyboard::new();
 
-    let c = cpu::Cpu::initialize();
+    let cpu = cpu::Cpu::initialize();
     let mut d = display::Display::new();
+    let c = cartridge::Cartridge::new(&args[1]);
+    print!("{:?}", c.rom);
 
     'gameloop: loop {
         d.canvas.clear();
@@ -25,9 +32,11 @@ fn main() {
                 _ => {}
             }
         }
-        // Do whatever and edit d.gfx to update the state of the next frame
-        d.set_frame();
 
+        // Do whatever, edit d.gfx to update the state of the next frame
+        // and set_frame to reflect changes onto the screen
+
+        d.set_frame();
         d.canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
